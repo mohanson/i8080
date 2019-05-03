f381072b201a1994f942e1ef742d21eaa5f545c6
mohanson <mohanson@outlook.com>
2019-05-03 18:58:57 +0800

    2019-05-03 18:58:57

---
+++ scripts/get_cpu_tests.py
@@ -0,0 +1,44 @@
+import contextlib
+import os.path
+
+import requests
+
+prefix = 'http://altairclone.com/downloads/cpu_tests/'
+suffix = [
+    '+README.TXT',
+    '8080EXER.COM',
+    '8080EXER.MAC',
+    '8080EXER.PNG',
+    '8080EXER.PRN',
+    '8080EXM.COM',
+    '8080EXM.MAC',
+    '8080EXM.PRN',
+    '8080PRE.COM',
+    '8080PRE.MAC',
+    '8080PRE.PRN',
+    '8080_8085 CPU Exerciser.pdf',
+    'CPUTEST.COM',
+    'TST8080.ASM',
+    'TST8080.COM',
+    'TST8080.PRN',
+]
+saveto = './res/cpu_tests'
+
+
+def main():
+    if not os.path.exists(saveto):
+        print(f'Make dir {saveto}')
+        os.makedirs(saveto, 0o755)
+    for e in suffix:
+        src = prefix + e
+        dst = os.path.join(saveto, e)
+        print(f'Get {src}')
+        resp = requests.get(src, stream=True)
+        with open(dst, 'wb') as f:
+            with contextlib.closing(resp) as r:
+                for data in r.iter_content(chunk_size=8 * 1024):
+                    f.write(data)
+
+
+if __name__ == '__main__':
+    main()
