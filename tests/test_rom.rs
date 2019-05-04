9cbc3e67e06606e20202b59dcc94b8060ecd7379
mohanson <mohanson@outlook.com>
2019-05-04 11:09:42 +0800

    2019-05-04 11:09:42

--- tests/test_rom.rs
+++ tests/test_rom.rs
@@ -12,15 +12,14 @@ fn load_test(mem: &mut help::Memory, path: impl AsRef<Path>) {
     println!("Test loaded: {:?} Bytes from {:?}", buf.len(), path.as_ref());
 }
 
-#[test]
-fn test_rom_8080pre() {
+fn exec_test(path: impl AsRef<Path>) {
     let mut mem = Box::new(help::Memory::new());
-    load_test(&mut mem, "./res/cpu_tests/8080PRE.COM");
+    load_test(&mut mem, path);
     let mut cpu = i8080::Cpu::power_up(mem);
     cpu.mem.set(0x0005, 0xc9);
-    cpu.reg.pc = 0xf800;
     cpu.reg.pc = 0x0100;
     loop {
+        let prev_pc = cpu.reg.pc;
         cpu.next();
         if cpu.reg.pc == 0x76 {
             panic!("")
@@ -31,7 +30,6 @@ fn test_rom_8080pre() {
                 loop {
                     let c = cpu.mem.get(a);
                     if c as char == '$' {
-                        println!("");
                         break;
                     } else {
                         a += 1;
@@ -44,7 +42,24 @@ fn test_rom_8080pre() {
             }
         }
         if cpu.reg.pc == 0x00 {
+            println!("");
+            println!("Jump to 0 from 0x{:04x}", prev_pc);
             break;
         }
     }
+}
+
+#[test]
+fn test_rom_8080pre() {
+    exec_test("./res/cpu_tests/8080PRE.COM");
+}
+
+#[test]
+fn test_rom_tst8080() {
+    exec_test("./res/cpu_tests/TST8080.COM");
+}
+
+#[test]
+fn test_rom_cputest() {
+    exec_test("./res/cpu_tests/CPUTEST.COM");
 }
